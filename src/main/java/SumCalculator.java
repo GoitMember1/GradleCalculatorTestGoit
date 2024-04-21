915282a76cdd5d4708c01a2522348f2cd2f6b4c0
MrRomashkaaa <mrromashkaofficial@gmail.com>
2024-04-21 11:41:01 +0200

    update

--- src/main/java/SumCalculator.java
+++ src/main/java/SumCalculator.java
@@ -1,7 +1,7 @@
 public class SumCalculator {
     public int sum(int n) {
-        if (n < 0) {
-            throw new IllegalArgumentException("n must be a non negative integer");
+        if (n <= 0) {
+            throw new IllegalArgumentException("n must be a positive integer");
         }
         return n * (n + 1) / 2;
     }
